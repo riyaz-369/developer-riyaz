8a2fdd4d218fae67c79c7dbf12e6fbb4f26fb2d9
Your Nameriyazulislam99 <riyazulislam0099@gmail.com>
2024-06-21 13:10:12 +0600

    banner image added

--- src/components/HomePage/Header/Banner.jsx
+++ src/components/HomePage/Header/Banner.jsx
@@ -1,11 +1,21 @@
 import { Button } from "@/components/ui/button";
 import { FiDownload } from "react-icons/fi";
+import bannerImg from "@/assets/banner/banner.png";
+import Image from "next/image";
+import { FaGithub, FaLinkedin, FaFacebook } from "react-icons/fa";
+import Link from "next/link";
 
 const Banner = () => {
+  const social = [
+    { icon: <FaGithub size={32} />, path: "/" },
+    { icon: <FaLinkedin size={32} />, path: "/" },
+    { icon: <FaFacebook size={32} />, path: "/" },
+  ];
+
   return (
-    <section>
+    <section className="bg-[#1b1c25]">
       <div className="container mx-auto h-full">
-        <div className="flex flex-col xl:flex-row items-center justify-between xl:pt-8 xl:pb-24">
+        <div className="flex flex-col xl:flex-row items-center justify-between">
           <div className="text-center xl:text-start">
             <span className="text-xl">Frontend Web Developer</span>
             <h1>
@@ -17,12 +27,36 @@ const Banner = () => {
             <p className="max-w-md mb-9 text-white/80">
               Building Dynamic User Interfaces with React.js
             </p>
-            <Button variant="outline">
-              <span>Download CV</span>
-              <FiDownload size={24} />
-            </Button>
+            <div className="flex flex-col xl:flex-row items-center gap-8">
+              <Button
+                variant="outline"
+                className="flex items-center gap-2 justify-center"
+              >
+                <span>Download CV</span>
+                <FiDownload size={16} />
+              </Button>
+              <div className="mb-8 xl:mb-0 flex gap-4 ">
+                {social.map((item, idx) => (
+                  <Link
+                    href={item.path}
+                    key={idx}
+                    className="hover:text-primary transition-all"
+                  >
+                    {item.icon}
+                  </Link>
+                ))}
+              </div>
+            </div>
           </div>
-          <div>photo</div>
+          <div className="flex justify-end items-end">
+            <Image
+              className=""
+              src={bannerImg}
+              alt=""
+              width={600}
+              quality={100}
+            />
+          </div>
         </div>
       </div>
     </section>
